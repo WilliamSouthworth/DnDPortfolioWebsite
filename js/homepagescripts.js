f78156faa550367649328b9004bd8b4ad8e7fc11
William <williamxsouthworth@gmail.com>
2026-03-25 17:15:37 -0400

    Organized files

---
+++ js/homepagescripts.js
@@ -0,0 +1,28 @@
+function toggleMenu() {
+    const navLinks = document.getElementById("nav_links");
+    navLinks.classList.toggle("show");
+}
+
+function CharSheet () {
+    alert("Character Sheet coming soon!");
+}
+
+function Backstory () {
+    alert("Backstory coming soon!");
+}
+
+function PreviousParties () {
+    alert("Previous Parties coming soon!");
+}
+
+function Quests () {
+    alert("Quests coming soon!");
+}
+
+function Reputation () {
+    alert("Reputation coming soon!");
+}
+
+function SendRaven () {
+    alert("Send Raven coming soon!");
+}
